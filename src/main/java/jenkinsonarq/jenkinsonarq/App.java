59749088e4a0e8811ad8e0e622bfd904c57b9d01
Admin <Admin@DESKTOP-QG2DJLM>
2022-11-30 05:45:46 +0530

    first commit to jenkinsonarq

---
+++ src/main/java/jenkinsonarq/jenkinsonarq/App.java
@@ -0,0 +1,13 @@
+package jenkinsonarq.jenkinsonarq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
